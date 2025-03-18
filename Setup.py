94291a3d6c3cb77bab5b8b75e66263a171e4c058
Program-dev12 <program-dev@hotmail.com>
2025-03-18 15:28:37 -0600

    Add files via upload

---
+++ Setup.py
@@ -0,0 +1,11 @@
+setup(
+   name ='pronuntation',
+   version='0.1',
+   packages=find_packages(),
+   description='A library for pronunciation in an easy and effective way.',
+   long_description=open('readme.md').read(),
+   autor='Iván David',
+   
+
+
+)
